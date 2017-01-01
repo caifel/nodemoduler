9f678ba19facc193104d277671925469393082d7
Mario Medrano <mariomedranonrd@gmail.com>
2016-12-31 22:12:36 -0300

    1. Refactor 2. Services deprecated. 3. Better inheritance management.

--- index.js
+++ index.js
@@ -12,54 +12,66 @@ module.exports = function(config) {
         var scopeBook = {};
         var schemaArgs = [];
         var folder = process.cwd() + '/' + config.folder + '/';
-        var keyWords = ['extend', 'mixin', 'service', 'require', 'model'];
+        var keyWords = ['extend', 'mixin', 'require', 'model'];
         var app = require(folder + config.main);
-        var contextualize = function (target, group, route) {
-            var scope;
-
-            if (_.isEmpty(route))
-                return false;
+        var contextualize = function (current, group) {
+            var next;
 
             switch (group) {
                 case 'extend':
-                    path = folder + route.split('.').join('/');
-                    scope = scopeBook[path] || require(path);
-                    intersection = _.intersection(_.functions(scope), _.functions(target));
-                    _.each(intersection, function (fnName) {
-                        target[fnName] = _.bind(target[fnName], _.extend(target, {
-                            callParent: scope[fnName]
-                        }));
+                    var road = [current];
+                    var ref = current.extend;
+                    while (ref) {
+                        path = folder + ref.split('.').join('/');
+                        next = scopeBook[path] || require(path);
+                        ref = next.extend;
+                        scopeManager(next);
+                        road.push(next);
+                        current = next;
+                    }
+                    _.each(road.reverse(), function (current, index, list) {
+                        if (index) {
+                            next = list[index - 1];
+                            intersection = _.intersection(_.functions(next), _.functions(current));
+                            _.each(intersection, function (fnName) {
+                                current[fnName] = _.bind(current[fnName], _.extend(current, {
+                                    callParent: next[fnName]
+                                }));
+                            });
+                            current.model = _.union(current.model, next.model);
+                            _.extend(current, _.omit(next, _.union(intersection, keyWords)));
+                        }
                     });
-                    _.extend(target, _.omit(scope, intersection));
                     break;
                 case 'model':
-                    path = folder + group + '/' +  route.split('.').join('/');
-                    scope = scopeBook[path] || require(path);
-                    target._schemas = target._schemas || [];
-                    target._schemas.push(scope['alias']);
-                    scope._isModel = true;
+                    _.each(current.model, function (ref) {
+                        path = folder + 'model/' +  ref.split('.').join('/');
+                        next = scopeBook[path] || require(path);
+                        next._isModel = true;
+                        next.alias = ref;
+                        scopeManager(next);
+                    });
                     break;
                 case 'mixin':
-                    path = folder + group + '/' + route.split('.').join('/');
-                    scope = scopeBook[path] || require(path);
-                    //scope._isMixin = true;
-                    intersection = _.intersection(_.functions(scope), _.functions(target));
-                    _.extend(target, _.omit(scope, intersection));
-                    break;
-                case 'service':
-                    path = folder + group + '/' + route.split('.').join('/');
-                    scope = scopeBook[path] || require(path);
-                    target[scope['alias']] = scope;
+                    _.each(current.mixin, function (ref) {
+                        path = folder + 'mixin/' +  ref.split('.').join('/');
+                        next = scopeBook[path] || require(path);
+                        intersection = _.intersection(_.functions(next), _.functions(current));
+                        _.extend(current, _.omit(next, intersection));
+                        scopeManager(next);
+                    });
                     break;
                 case 'require':
-                    path = folder + route.split('.').join('/');
-                    scope = scopeBook[path] || require(path);
-                    target[scope['alias']] = scope;
+                    _.each(current.require, function (ref) {
+                        path = folder + '/' +  ref.split('.').join('/');
+                        next = scopeBook[path] || require(path);
+                        current[scope['alias']] = next;
+                        scopeManager(next);
+                    });
                     break;
                 default:
                     return;
             }
-            scopeManager(scope);
         };
         var scopeManager = function(target) {
             if (_.has(scopeBook, path))
@@ -67,15 +79,9 @@ module.exports = function(config) {
             scopeBook[path] = target;
 
             _.each(keyWords, function (group) {
-                if (_.isArray(target[group]))
-                    _.each(target[group], function (route) {
-                        contextualize(target, group, route)
-                    });
-                else
-                    contextualize(target, group, target[group]);
-                delete target[group];
+                _.has(target, group) && contextualize(target, group);
             });
-        };        
+        };
         var initMiddleWare = function () {
             path = folder + 'middleware';
             var middleware = require(path);
@@ -109,7 +115,7 @@ module.exports = function(config) {
         var initResources = function () {
             _.each(app.resource, function (refs, route) {
                 path = folder + 'resource/' + route.split('.').join('/');
-                resource = require(path);                
+                resource = require(path);
                 scopeManager(resource);
 
                 _.each(refs, function (info, src) {
@@ -148,25 +154,16 @@ module.exports = function(config) {
                         console.error('The model: "' + target['alias'] + '" does not have an schema method');
                     delete target['alias'];
                 }
-                /**
-                 * Mixin can have an init method, but it won't be executed at the start.
-                 * */
-                /*if (target._isMixin) {
-                    delete target._isMixin;
-                    delete scopeBook[path];
-                }*/
             });
             /**
              * Step 2:
              *      Pass the schema(s) to all those who have required it.
              * */
             _.each(scopeBook, function (target) {
-                if (_.has(target, '_schemas')) {
-                    _.each(target._schemas, function (ref) {
+                if (_.has(target, 'model'))
+                    _.each(target.model, function (ref) {
                         target[ref] = schemas[ref];
                     });
-                    delete target._schemas;
-                }
             });
             schemas = {}; /** No longer needed */
             /**
